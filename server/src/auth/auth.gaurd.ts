f2f6a2e9e59c7916990383370018ad934ce888e5
Azzazy6269 <mohamadelazzazy@gmail.com>
2026-08-14 18:46:05 +0300

    fix: auth guard fetch user data from db instead of token payload to avoid changing data while it's fixed in token

--- server/src/auth/auth.gaurd.ts
+++ server/src/auth/auth.gaurd.ts
@@ -5,10 +5,13 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthGaurd implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
@@ -24,7 +27,75 @@ export class AuthGaurd implements CanActivate {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: process.env.JWT_SECRET,
       });
-      request['user'] = payload;
+
+      const user = await this.prisma.user.findUnique({
+        where: { id: payload.id },
+        select: {
+          id: true,
+          email: true,
+          fullName: true,
+          avatarUrl: true,
+
+          roleId: true,
+          role: {
+            select: {
+              id: true,
+              key: true,
+              name: true,
+              permissions: {
+                select: {
+                  permission: {
+                    select: {
+                      id: true,
+                      key: true,
+                      name: true,
+                    },
+                  },
+                },
+              },
+            },
+          },
+
+          organizationId: true,
+          organization: {
+            select: {
+              id: true,
+              name: true,
+              logoUrl: true,
+            },
+          },
+
+          isSuperAdmin: true,
+        },
+      });
+
+      if(!user){
+        throw new UnauthorizedException({
+          success: false,
+          error: 'Invalid or expired token',
+        });
+      }
+      const data = {
+        id: user.id,
+        email: user.email,
+        fullName: user.fullName,
+        avatarUrl: user.avatarUrl,
+
+        roleId: user.roleId,
+        role: user.role?.name,
+
+        organizationId: user.organizationId,
+        organization: user.organization?.name,
+
+        organizationLogo: user.organization?.logoUrl,
+
+        isSuperAdmin: user.isSuperAdmin,
+
+        iat: payload.iat,
+        exp: payload.exp,
+      };
+
+      request['user'] = data;
       return true;
     } catch (error) {
       throw new UnauthorizedException({
